ea0f75a5a81fd17992fb883a94536ef4f045e0da
hongjin4790 <904rla@naver.com>
2022-01-20 20:51:19 +0900

    Doggler 프로젝트

--- Kotlin/Codelabs/Layouts/DisplayScrollableList/Project_Doggler/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/dogglers/databinding/GridListItemBinding.java
+++ Kotlin/Codelabs/Layouts/DisplayScrollableList/Project_Doggler/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/dogglers/databinding/GridListItemBinding.java
@@ -4,6 +4,8 @@ package com.example.dogglers.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewbinding.ViewBinding;
@@ -11,13 +13,32 @@ import com.example.dogglers.R;
 import com.google.android.material.card.MaterialCardView;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class GridListItemBinding implements ViewBinding {
   @NonNull
   private final MaterialCardView rootView;
 
-  private GridListItemBinding(@NonNull MaterialCardView rootView) {
+  @NonNull
+  public final TextView itemDogAge;
+
+  @NonNull
+  public final TextView itemDogHobbies;
+
+  @NonNull
+  public final TextView itemDogName;
+
+  @NonNull
+  public final ImageView itemImage;
+
+  private GridListItemBinding(@NonNull MaterialCardView rootView, @NonNull TextView itemDogAge,
+      @NonNull TextView itemDogHobbies, @NonNull TextView itemDogName,
+      @NonNull ImageView itemImage) {
     this.rootView = rootView;
+    this.itemDogAge = itemDogAge;
+    this.itemDogHobbies = itemDogHobbies;
+    this.itemDogName = itemDogName;
+    this.itemImage = itemImage;
   }
 
   @Override
@@ -43,10 +64,38 @@ public final class GridListItemBinding implements ViewBinding {
 
   @NonNull
   public static GridListItemBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.item_dog_age;
+      TextView itemDogAge = rootView.findViewById(id);
+      if (itemDogAge == null) {
+        break missingId;
+      }
 
-    return new GridListItemBinding((MaterialCardView) rootView);
+      id = R.id.item_dog_hobbies;
+      TextView itemDogHobbies = rootView.findViewById(id);
+      if (itemDogHobbies == null) {
+        break missingId;
+      }
+
+      id = R.id.item_dog_name;
+      TextView itemDogName = rootView.findViewById(id);
+      if (itemDogName == null) {
+        break missingId;
+      }
+
+      id = R.id.item_image;
+      ImageView itemImage = rootView.findViewById(id);
+      if (itemImage == null) {
+        break missingId;
+      }
+
+      return new GridListItemBinding((MaterialCardView) rootView, itemDogAge, itemDogHobbies,
+          itemDogName, itemImage);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
