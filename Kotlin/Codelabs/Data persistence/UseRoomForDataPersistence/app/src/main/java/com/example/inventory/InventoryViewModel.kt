a35b187f08063419b1b85e8058443ba48d237cae
hongjin4790 <904rla@naver.com>
2022-02-11 18:01:01 +0900

    room으로 데이터유지하기

--- Kotlin/Codelabs/Data persistence/UseRoomForDataPersistence/app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++ Kotlin/Codelabs/Data persistence/UseRoomForDataPersistence/app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -1,21 +1,22 @@
 package com.example.inventory
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
+import androidx.navigation.fragment.NavHostFragment.findNavController
+
 import com.example.inventory.data.Item
 import com.example.inventory.data.ItemDao
 import kotlinx.coroutines.launch
 import java.lang.IllegalArgumentException
 
-class InventoryViewModel(private val itemDao: ItemDao): ViewModel(){
-    private fun insertItem(item: Item){
+class InventoryViewModel(private val itemDao: ItemDao): ViewModel() {
+    val allItems: LiveData<List<Item>> = itemDao.getItems().asLiveData()
+    private fun insertItem(item: Item) {
         viewModelScope.launch {
             itemDao.insert(item)
         }
     }
 
-    private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount:String):Item{
+    private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String): Item {
         return Item(
             itemName = itemName,
             itemPrice = itemPrice.toDouble(),
@@ -23,17 +24,63 @@ class InventoryViewModel(private val itemDao: ItemDao): ViewModel(){
         )
     }
 
-    fun addNewItem(itemName: String, itemPrice: String, itemCount: String){
-        val newItem = getNewItemEntry(itemName,itemPrice, itemCount)
+    fun addNewItem(itemName: String, itemPrice: String, itemCount: String) {
+        val newItem = getNewItemEntry(itemName, itemPrice, itemCount)
         insertItem(newItem)
     }
 
-    fun isEntryValid(itemName: String, itemPrice: String, itemCount: String): Boolean{
-        if(itemName.isBlank() || itemPrice.isBlank() || itemCount.isBlank())
-        {
+    fun isEntryValid(itemName: String, itemPrice: String, itemCount: String): Boolean {
+        if (itemName.isBlank() || itemPrice.isBlank() || itemCount.isBlank()) {
             return false
         }
         return true
+    }
+
+    fun retrieveItem(id: Int): LiveData<Item> {
+        return itemDao.getItem(id).asLiveData()
+    }
+
+    private fun updateItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.update(item)
+        }
+    }
+
+    fun sellItem(item: Item) {
+        if (item.quantityInStock > 0) {
+            val newItem = item.copy(quantityInStock = item.quantityInStock - 1)
+            updateItem(newItem)
+        }
+    }
+
+    fun deleteItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.delete(item)
+        }
+    }
+
+    private fun getUpdatedItemEntry(
+        itemId: Int,
+        itemName: String,
+        itemPrice: String,
+        itemCount: String
+    ): Item {
+        return Item(
+            id = itemId,
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
+
+    fun updateItem(
+        itemId: Int,
+        itemName: String,
+        itemPrice: String,
+        itemCount: String
+    ){
+        val updatedItem = getUpdatedItemEntry(itemId,itemName, itemPrice, itemCount)
+        updateItem(updatedItem)
     }
 }
 
