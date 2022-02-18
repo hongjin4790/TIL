5ea4e1c0d72883a3001d862302e6e15154b1abcd
hongjin4790 <904rla@naver.com>
2022-02-18 21:56:13 +0900

    데이터 읽고 쓰기

--- Kotlin/ConnectFirebase/app/src/main/java/com/example/firebasestudy/MainActivity.kt
+++ Kotlin/ConnectFirebase/app/src/main/java/com/example/firebasestudy/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.firebasestudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.firebasestudy.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "KotlinActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+        
+        myRef.addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.getValue<String>()
+                Log.d(TAG, "value is: $value ")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+
+        })
+        
+
     }
 }
